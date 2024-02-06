8016ffb4051500b317b46a3926891960b740d3c0
Riya Sinha <67583070+wisteriyas@users.noreply.github.com>
2024-02-06 09:31:39 +0000

    Practice of Python Operations

--- Start/Ch1/filteringdata.py
+++ Start/Ch1/filteringdata.py
@@ -10,12 +10,22 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
 
 # the filter() function gives us a way to remove unwanted data points
 # TODO: create a subset of the data for days that had snowfall
-
+# snowdays = list(filter(lambda d: d['snow'] > 0.0, weatherdata))
+# print(len(weatherdata))
+# print(len(snowdays))
 
 # TODO: pretty-print the resulting data set
-
+# pprint.pp(snowdays)
 
 # filter can also be used on non-numerical data, like strings
 # TODO: create a subset that contains summer days with heavy rain (more than 1 in, about 2.5cm)
 def is_summer_rain_day(d):
+    summer_months = ["-07-","-08-"]
+    if any([m in d['date'] for m in summer_months]) and d['prcp']>=1.0:
+        return True
     return False
+
+summerraindays = list(filter(is_summer_rain_day, weatherdata))
+
+print(len(summerraindays))
+pprint.pp(summerraindays)
--- Start/Ch1/parsedata.py
+++ Start/Ch1/parsedata.py
@@ -1,14 +1,27 @@
 # Example file for Advanced Python: Hands On by Joe Marini
 # Load and parse a JSON data file and determine some information about it
 
+import json
+import pprint
 
 # TODO: open the sample weather data file and use the json module to load and parse it
-
+with open("../../sample-weather-history.json","r") as weatherfile:
+    weatherdata = json.load(weatherfile)
 
 # TODO: make sure the data loaded correctly by printing the length of the dataset
+# print(len(weatherdata))
 
 # TODO: let's also take a look at the first item in the data
-
+# pprint.pp(weatherdata[0])
 
 # TODO: How many days of data do we have for each year?
+years = {}
 
+for d in weatherdata:
+    key = d["date"][0:4]
+    if key in years:
+        years[key] += 1
+    else:
+        years[key] = 1
+
+pprint.pp(years, width=5)
